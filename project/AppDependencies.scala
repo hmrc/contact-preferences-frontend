8fedfc4a75e70341cc082be31b2ddc79d5ba71c7
Durga <durga.kandula@digital.hmrc.gov.uk>
2019-02-01 09:41:49 +0000

    authentication services added to contact prferences frontend

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,6 +1,4 @@
 import play.core.PlayVersion.current
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
@@ -13,12 +11,15 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "org.scalatest"           %% "scalatest"                % "3.0.4"                 % "test",
-    "org.jsoup"               %  "jsoup"                    % "1.10.2"                % "test",
-    "com.typesafe.play"       %% "play-test"                % current                 % "test",
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
-    "uk.gov.hmrc"             %% "service-integration-test" % "0.2.0"                 % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.0"                 % "test, it"
+    "uk.gov.hmrc"             %% "hmrctest"                    % "3.4.0-play-25"         % "test, it",
+    "org.scalatest"           %% "scalatest"                   % "3.0.4"                 % "test",
+    "com.typesafe.play"       %% "play-test"                   % current                 % "test",
+    "org.pegdown"             %  "pegdown"                     % "1.6.0"                 % "test, it",
+    "uk.gov.hmrc"             %% "service-integration-test"    % "0.2.0"                 % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"          % "2.0.0"                 % "test, it",
+    "org.scalamock"           %% "scalamock-scalatest-support" % "3.6.0"                 % "test",
+    "org.mockito"             %  "mockito-core"                % "2.13.0"                % "test",
+    "com.github.tomakehurst"  %  "wiremock"                    % "2.6.0"                 % "it"
   )
 
 }
---
+++ test/controllers/ContactPreferencesFrontendAuthorisedSpec.scala
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import assets.BaseTestConstants.testVatNumber
+import config.Constants
+import connectors.mocks.MockAuthConnector
+import controllers.actions.ContactPreferencesFrontendAuthorised
+import play.api.http.Status._
+import play.api.mvc.Result
+import play.api.mvc.Results._
+import uk.gov.hmrc.auth.core.authorise.Predicate
+import uk.gov.hmrc.auth.core.{Enrolment, InsufficientEnrolments, MissingBearerToken}
+
+import scala.concurrent.Future
+
+
+class ContactPreferencesFrontendAuthorisedSpec extends MockAuthConnector {
+
+  object TestContactPreferencesAuthorised extends ContactPreferencesFrontendAuthorised(mockAuthConnector)
+
+  def result: Future[Result] = TestContactPreferencesAuthorised.async(testVatNumber) {
+    implicit user =>
+      Future.successful(Ok)
+  }(ec)(fakeRequest)
+
+  val authPredicate: Predicate =
+    Enrolment(Constants.MtdContactPreferencesEnrolmentKey)
+      .withIdentifier(Constants.MtdContactPreferencesReferenceKey, testVatNumber)
+      .withDelegatedAuthRule(Constants.MtdContactPreferencesDelegatedAuth)
+
+  "The ContactPreferencesAuthorised.async method" should {
+
+    "For a Principal User" when {
+
+      "an authorised result is returned from the Auth Connector" should {
+
+        "Successfully authenticate and process the request" in {
+          mockAuthRetrieveMtdVatEnrolled(authPredicate)
+          status(result) shouldBe OK
+        }
+      }
+    }
+
+    "For an Agent User" when {
+
+      "they are Signed Up to MTD ContactPreferences" should {
+
+        "Successfully authenticate and process the request" in {
+          mockAuthRetrieveAgentServicesEnrolled(authPredicate)
+          status(result) shouldBe OK
+        }
+      }
+    }
+
+    "For any type of user" when {
+
+      "a NoActiveSession exception is returned from the Auth Connector" should {
+
+        "Return a unauthorised response" in {
+          mockAuthorise(authPredicate, retrievals)(Future.failed(MissingBearerToken()))
+          status(result) shouldBe UNAUTHORIZED
+        }
+      }
+
+      "an InsufficientAuthority exception is returned from the Auth Connector" should {
+
+        "Return a forbidden response" in {
+          mockAuthorise(authPredicate, retrievals)(Future.failed(InsufficientEnrolments()))
+          status(result) shouldBe FORBIDDEN
+        }
+      }
+    }
+  }
+}
