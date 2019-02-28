d0ba35d80004b80070417b364a99b430996482c6
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2019-02-28 09:15:34 +0000

    [store-journey-preference] Added TestOnly routes to enable simulated journey from calling service

---
+++ app/testOnly/controllers/SimulateJourneyController.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package testOnly.controllers
+
+import config.AppConfig
+import javax.inject.{Inject, Singleton}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent}
+import testOnly.connectors.ContactPreferencesConnector
+import testOnly.connectors.httpParsers.SimulateJourneySubmitHttpParser.Success
+import testOnly.forms.SimulateJourneyForm.simulateJourneyForm
+import testOnly.views.html.{simulate_journey_start, simulate_journey_success}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+
+import scala.concurrent.Future
+
+@Singleton
+class SimulateJourneyController @Inject()(contactPreferenceConnector: ContactPreferencesConnector,
+                                          val messagesApi: MessagesApi,
+                                          implicit val appConfig: AppConfig) extends FrontendController with I18nSupport {
+
+  val startJourneyShow: Action[AnyContent] = Action { implicit request =>
+    Ok(simulate_journey_start(simulateJourneyForm, testOnly.controllers.routes.SimulateJourneyController.startJourneySubmit()))
+  }
+
+  val success: Action[AnyContent] = Action.async { implicit request =>
+    contactPreferenceConnector.getPreference(request.session.get("CONTACT_PREFS_JOURNEY_ID").getOrElse("")).map {
+      case Right(data) => Ok(simulate_journey_success(data))
+      case Left(err) => Status(err.status)(err.body)
+    }
+  }
+
+  val startJourneySubmit: Action[AnyContent] = Action.async { implicit request =>
+    simulateJourneyForm.bindFromRequest.fold(
+      formWithErrors =>
+        Future.successful(BadRequest(simulate_journey_start(formWithErrors, testOnly.controllers.routes.SimulateJourneyController.startJourneySubmit()))),
+      data => {
+        contactPreferenceConnector.startJourney(data).map {
+          case Right(Success(url)) => Redirect(url).addingToSession("CONTACT_PREFS_JOURNEY_ID" -> url.split("/").last)
+          case Left(err) => Status(err.status)(err.body)
+        }
+      }
+    )
+  }
+}
