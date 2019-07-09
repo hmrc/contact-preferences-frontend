87fcb241c5e903211d4dc62b4023413ece83244a
r-melvin <40235254+r-melvin@users.noreply.github.com>
2019-07-09 12:17:09 +0100

    SAR-4154: Dependency uplift

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,12 +6,12 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"             %% "play-whitelist-filter"    % "2.0.0",
     "uk.gov.hmrc"             %% "govuk-template"           % "5.26.0-play-25",
-    "uk.gov.hmrc"             %% "play-ui"                  % "7.33.0-play-25",
-    "uk.gov.hmrc"             %% "bootstrap-play-25"        % "4.9.0"
+    "uk.gov.hmrc"             %% "play-ui"                  % "7.40.0-play-25",
+    "uk.gov.hmrc"             %% "bootstrap-play-25"        % "4.13.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "hmrctest"                    % "3.6.0-play-25"         % "test, it",
+    "uk.gov.hmrc"             %% "hmrctest"                    % "3.9.0-play-25"         % "test, it",
     "org.scalatest"           %% "scalatest"                   % "3.0.4"                 % "test",
     "org.jsoup"               %  "jsoup"                       % "1.10.2"                % "test",
     "com.typesafe.play"       %% "play-test"                   % current                 % "test",
